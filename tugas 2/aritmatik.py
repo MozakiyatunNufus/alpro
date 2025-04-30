1e3b75afa2cc0fc158d4372ced8db8c8a6a13f66
MozakiyatunNufus <mozakiyatunnufus05@gmail.com>
2025-04-30 20:22:46 +0700

    Add files via upload

---
+++ tugas 2/aritmatik.py
@@ -0,0 +1,39 @@
+# buat 2 variabel
+a = 5
+b = 3
+
+# penjumlahan(+)
+hasil = a + b
+print('Hasil penjumlahan : ',hasil)
+
+# pengurangan(-)
+hasil = a - b
+print('Hasil pengurangan : ',hasil)
+
+# perkalian(*)
+hasil = a * b
+print('Hasil perkalian :',hasil)
+
+# pangkat(**)
+hasil = a ** b
+print('Hasil pengkat :',hasil)
+
+# pembagian(/)
+hasil = a / b
+print('Hasil pembagian :',hasil)
+
+# pembagian ke bawah(//)
+hasil = a // b
+print('Hasil pembagian kebawah :',hasil)
+
+# sisa bagi(%)
+hasil = a % b
+print('Hasil sisa bagi :',hasil)
+
+# precedence
+hasil = 1 + 2 * 4 - 3
+print('Hasil precedence :',hasil)
+
+# precedence
+hasil = (1 + 2) * 4 - 3
+print('Hasil precedence :',hasil)
---
+++ tugas 2/logical.py
@@ -0,0 +1,18 @@
+print('OPERATOR AND')
+print('Semua statement benar :',
+       (5>3) and (7>5))
+print('Salah satu statement salah :', 
+       (5<3) and (7>5))
+print('--------------')
+
+print('OPERATOR OR')
+print('Semua statement benar :',
+        (5<3) or (7<5))
+print('Salah satu statement salah :', 
+        (5<3) or (7>5))
+print('---------------')
+
+print('OPERATOR NOT')
+print('Semua statement benar :',not(5>3))
+print('Salah satu statement salah :',not(5<3))
+print('--------------')
