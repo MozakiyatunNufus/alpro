24c1b5fa1c3647bbaaef047b9af1e46697e7ebab
MozakiyatunNufus <mozakiyatunnufus05@gmail.com>
2025-04-30 20:23:45 +0700

    Add files via upload

---
+++ tugas 7/module.py
@@ -0,0 +1,11 @@
+# call module 
+# import hitung # all function
+from hitung import jumlah
+
+# input 
+angka1 = int(input('Masukkan angka 1 : '))
+angka2 = int(input('Masukkan angka 2 : '))
+
+# call function
+# hitung.kali(angka1,angka2)
+jumlah(angka1,angka2)
